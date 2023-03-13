9dbe1ec30b9fa573364dbce99a165f422757a59b
Eilidh Morone <e.silendt@gmail.com>
2023-03-13 12:22:21 +0000

    set up boiler plate, initalized firebase, set up bootstrap, created sign up page component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignUp from "./components/auth/SignUp";
+import {Container} from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>This is Check Please App Page</h1>
+      <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"60vh"}}>
+        <SignUp/>
+      </Container>
     </div>
   );
 }
---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import "firebase/compat/auth";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// Initialize Firebase
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+export const auth = app.auth();
+export default app;
