743450e36c2562dd8ac2daa6fe8f9941794833e5
Eilidh Morone <e.silendt@gmail.com>
2023-03-15 16:33:35 +0000

    created Benefit class

---
+++ BackEnd/src/main/java/com/example/java_practice/models/Benefit.java
@@ -0,0 +1,42 @@
+package com.example.java_practice.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="benefits")
+public class Benefit {
+    //INSTANCE VARIABLES:
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name ="benefitType")
+    private String benefitType;
+
+    //CONSTRUCTOR:
+    public Benefit(Long id, String benefitType) {
+        this.id = id;
+        this.benefitType = benefitType;
+    }
+
+    //EMPTY CONSTRUCTOR:
+    public Benefit() {
+    }
+
+    //GETTERS AND SETTERS:
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBenefitType() {
+        return benefitType;
+    }
+
+    public void setBenefitType(String benefitType) {
+        this.benefitType = benefitType;
+    }
+}
