67e52aa26c46f6be72b9919d7775c24647055f3f
Fraser <fraser.russell1991@gmail.com>
2023-03-15 15:27:17 +0000

    NavBar now at top of page, removed testing messages for signed in/signed out, tidied up code in all the layout files

--- FrontEnd/src/components/layout/SignedOutLinks.js
+++ FrontEnd/src/components/layout/SignedOutLinks.js
@@ -2,29 +2,19 @@ import React from 'react';
 import {NavLink, useNavigate} from 'react-router-dom';
 import {Button} from "react-bootstrap"
 import { useAuth } from '../../contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 const SignedOutLinks = () => {    
-    const navigate = useNavigate()
-    const {logout} = useAuth()
-    
-    async function handleClick(e) {
-        e.preventDefault()
-        try{
-           await logout() 
-           navigate("/signin")
-        } catch{
-            return "Failed to logout"
-        }
-
-    }
    return (
-       <ul className='temp'>
-        we are rendering signed out links
-        <li><NavLink to='/signup'>Sign up</NavLink></li>
-        <li><NavLink to='/signin'>Sign in</NavLink></li>
-        <Button variant="link" onClick={handleClick}>Log Out</Button>
+       <>
+        <Button variant="outline-primary" as={Link} to="/signup">
+              Sign Up
+        </Button>
+        <Button variant="primary" as={Link} to="/signin" className="ms-2">
+            Sign In
+        </Button>
         {/* <li><NavLink to='/' className='temp'>NN</NavLink></li> */}
-       </ul>
+       </>
    )
 }
 export default SignedOutLinks
