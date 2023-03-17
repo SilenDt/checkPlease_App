9ffca6bd62f96965b76c25cafafa7f184dd0d9b9
Victoria <vicki_ng92@yahoo.com>
2023-03-17 16:53:05 +0000

    toggle tabs

--- FrontEnd/src/components/pages/CompanyDetail.js
+++ FrontEnd/src/components/pages/CompanyDetail.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router"
 import { useState, useEffect } from "react";
 import { getOneCompany } from "../../services/CompanyServices";
+import { Tab, Tabs, Image } from "react-bootstrap";
 
 const CompanyDetail = ({ companiesInfo, selectedCompany }) => {
     const { id } = useParams()
@@ -10,71 +11,34 @@ const CompanyDetail = ({ companiesInfo, selectedCompany }) => {
 
     return (
         <>
-            <div>
+        <Image
+            src="https://picsum.photos/900/400"
+            fluid
 
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
+        />
 
-                        <button class="nav-link active" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tablist" aria-controls="overview" aria-selected="true">Overview</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="reviews-tab" data-bs-toggle="tab" data-bs-target="#reviews" type="button" role="tablist" aria-controls="reviews" aria-selected="false">Reviews</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="wages-tab" data-bs-toggle="tab" data-bs-target="#wages" type="button" role="tablist" aria-controls="wages" aria-selected="false">Wages</button>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab"> <br></br>
+            <Tabs
+                defaultActiveKey="Overview"
+                id="uncontrolled-tab-example"
+                className="mb-3"
+                >
+                <Tab eventKey="overview" title="Overview" >
                         {oneCompany.name} <br></br>
                         {oneCompany.town} <br></br>
                         {oneCompany.rating} <br></br>
                         {oneCompany.description} <br></br>
                         Website: <br></br>
                         Email:
-                    </div>
-                    <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
-                        <h2>Reviews</h2>
-                        <p>lorekjfbjkfkdskdamda;,d,da kakkdakankdaklad aljlaald</p>
-                    </div>
-                    <div class="tab-pane fade" id="wages" role="tabpanel" aria-labelledby="wages-tab">Wages</div>
-                </div>
-            </div>
+                </Tab>
+                <Tab eventKey="reviews" title="Reviews">
+                    blaaaaaaaaaaa
+                </Tab>
+                <Tab eventKey="wages" title="Wages">
+                    dollllaaaaassssss $$$$
+                </Tab>
+                </Tabs>
+                </>
 
-        </>
-        // <>
-        //     <div>
-
-        //         <ul class="nav nav-tabs" id="myTab" role="tablist">
-        //             <li class="nav-item" role="presentation">
-
-        //                 <button class="nav-link active" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tablist" aria-controls="overview" aria-selected="true">Overview</button>
-        //             </li>
-        //             <li class="nav-item" role="presentation">
-        //                 <button class="nav-link" id="reviews-tab" data-bs-toggle="tab" data-bs-target="#reviews" type="button" role="tablist" aria-controls="reviews" aria-selected="false">Reviews</button>
-        //             </li>
-        //             <li class="nav-item" role="presentation">
-        //                 <button class="nav-link" id="wages-tab" data-bs-toggle="tab" data-bs-target="#wages" type="button" role="tablist" aria-controls="wages" aria-selected="false">Wages</button>
-        //             </li>
-        //         </ul>
-        //         <div class="tab-content" id="myTabContent">
-        //             <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab"> <br></br>
-        //             {oneCompany.name} <br></br>
-        //             {oneCompany.town} <br></br>
-        //             {oneCompany.rating} <br></br>
-        //             {oneCompany.description} <br></br>
-        //             Website: <br></br>
-        //             Email:
-        //             </div>
-        //             <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
-        //                 <h2>Reviews</h2>
-        //                 <p>lorekjfbjkfkdskdamda;,d,da kakkdakankdaklad aljlaald</p>
-        //             </div>
-        //             <div class="tab-pane fade" id="wages" role="tabpanel" aria-labelledby="wages-tab">Wages</div>
-        //         </div>
-        //     </div>
-
-        // </>
     );
 }
 
