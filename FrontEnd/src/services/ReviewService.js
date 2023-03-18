4017944e59cdb8bcd015da169acc3f1bc4670fa7
user <lauraamcg@gmail.com>
2023-03-18 09:50:14 +0000

    added a service page

--- FrontEnd/src/services/ReviewService.js
+++ FrontEnd/src/services/ReviewService.js
@@ -1,11 +1,30 @@
-const baseURL = "http://localhost:8080"
+const baseURL = "http://localhost:8080/reviews"
 
 export  function getReviewsInfo(){
-    return  fetch(baseURL + "/reviews")
+    return  fetch(baseURL)
     .then(res => res.json())
 }
 
 export async function getOneReview(id){
-    return await fetch(baseURL + "/reviews" + id)
+    return await fetch(baseURL + id)
     .then(res => res.json())
 }
+
+export function addAReview(){
+    return fetch(baseURL)
+}
+
+
+await fetch('/api/questions', {
+    method: 'POST',
+    body: JSON.stringify({
+    tipOutMultipleChoichDropdown,
+    yesOrNo,
+    shortAnswer,
+    multipleAnswers,
+    jobTitletMultipleChoichDropdown,
+}),
+headers: {
+    'Content-Type': 'application/json',
+    },
+});
