8036bcb3cb398d187486830f64424daf4fcff161
user <lauraamcg@gmail.com>
2023-03-21 16:35:02 +0000

    deleted unecessary stuff from backend

--- BackEnd/src/main/java/com/example/java_practice/repositories/UserDtoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.java_practice.repositories;
-
-import com.example.java_practice.models.UserDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDtoRepository extends JpaRepository<UserDto, Long> {
-
-
-}
